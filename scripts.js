5bf75d6256cd02087a06e9842fe1439a378804f6
Liz Baldelomar <hello@lizethb.com>
2023-10-12 23:39:21 +0800

    Add more input validation and error handling

--- scripts.js
+++ scripts.js
@@ -7,9 +7,14 @@ function Calculator() {
   };
 
   this.calculate = function(arr) {
-    if (arr.length === 3 && arr[1] != '' && !isNaN(+arr[0]) && !isNaN(+[2])) {
+    if (
+      arr.length === 3
+      && (arr[1] === '+' || arr[1] === '−' || arr[1] === '×' || arr[1] === '÷')
+      && !isNaN(+arr[0])
+      && !isNaN(+[2])
+      ) {
       return String(this.operations[arr[1]](+arr[0], +arr[2]));
-    } else if (arr.length === 1 && !isNaN(+arr[0])) {
+    } else if ((arr.length === 1 || arr.length === 2) && !isNaN(+arr[0])) {
       return arr[0];
     } else {
       return NaN;
@@ -42,15 +47,26 @@ function parseDisplay(str) {
   if (isValidOperator(oper)) {
     input.push(str.slice(0, str.indexOf(oper)));
     input.push(oper);
-    input.push(str.slice(str.indexOf(oper) + 1, str.length));
+    if (str.indexOf(oper) + 1 < str.length) {
+      input.push(str.slice(str.indexOf(oper) + 1, str.length));
+    }
+    console.log(input);
     return input;
   } else {
+    console.log([str]);
     return [str];
   }
 }
 
 const calc = new Calculator;
 const display = document.querySelector('[data-display]');
+
+const errorMsgs = ['What are you doing?', 
+  'I can\'t compute that!',
+  'Are you for real?',
+  'I\'ll pretend I didn\'t see that.',
+  'Excuse me?',
+  'You\'re not even making sense!'];
 
 const ac = document.querySelector('[data-ac]');
 ac.addEventListener('click', () => display.textContent = '0');
@@ -72,9 +88,11 @@ nums.forEach((num) => {
     if (!isNaN(+e.target.innerText)) {
       if (display.textContent === '0') {
         display.textContent = e.target.innerText;
-      } else {
+      } else if (!errorMsgs.includes(display.textContent)) {
         display.textContent += e.target.innerText;
       }
+    } else if (!errorMsgs.includes(display.textContent)) {
+      display.textContent = errorMsgs[Math.floor(Math.random() * 5)];
     }
   });
 });
@@ -82,7 +100,7 @@ nums.forEach((num) => {
 const operators = document.querySelectorAll('[data-oper]');
 operators.forEach((oper) => {
   oper.addEventListener('click', (e) => {
-    if (isValidOperator(e.target.innerText)) {
+    if (isValidOperator(e.target.innerText) && !errorMsgs.includes(display.textContent)) {
       let currentOper = findOperator(display.textContent);
       if (currentOper === '') {
         display.textContent += e.target.innerText;
@@ -95,11 +113,15 @@ operators.forEach((oper) => {
         display.textContent = calc.calculate(parseDisplay(display.textContent));
         display.textContent += e.target.innerText;
       }
+    } else if (!errorMsgs.includes(display.textContent)) {
+      display.textContent = errorMsgs[Math.floor(Math.random() * 5)];
     }
   });
 });
 
 const equals = document.querySelector('[data-equal]');
 equals.addEventListener('click', () => {
-  display.textContent = calc.calculate(parseDisplay(display.textContent));
+  if (!errorMsgs.includes(display.textContent)) {
+    display.textContent = calc.calculate(parseDisplay(display.textContent));
+  }
 });
